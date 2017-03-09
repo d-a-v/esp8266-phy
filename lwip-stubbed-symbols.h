463e75fbe8b3a992ee76a4b2732306267b4a6057
David Gauchard <gauchard@laas.fr>
2017-03-09 22:59:31 +0100

    esp/lwip: git-clone-lwip :) winter is coming

---
+++ lwip-stubbed-symbols.h
@@ -0,0 +1,35 @@
+#ifndef LWIP_STUBBED_SYMBOLS_H
+#define LWIP_STUBBED_SYMBOLS_H
+
+//< lwip-linked-symbols-blob-list.txt sed 's,\(.*\),#define \1 \1_STUBBED,g'
+
+// these symbols must be renamed in the new implementations
+// if ever they exist
+
+#define dhcp_cleanup dhcp_cleanup_STUBBED
+#define dhcp_release dhcp_release_STUBBED
+#define dhcp_start dhcp_start_STUBBED
+#define dhcp_stop dhcp_stop_STUBBED
+#define dhcps_start dhcps_start_STUBBED
+#define dhcps_stop dhcps_stop_STUBBED
+#define espconn_init espconn_init_STUBBED
+#define etharp_output etharp_output_STUBBED
+#define ethbroadcast ethbroadcast_STUBBED
+#define ethernet_input ethernet_input_STUBBED
+#define lwip_init lwip_init_STUBBED
+#define netif_add netif_add_STUBBED
+#define netif_default netif_default_STUBBED
+#define netif_remove netif_remove_STUBBED
+#define netif_set_addr netif_set_addr_STUBBED
+#define netif_set_default netif_set_default_STUBBED
+#define netif_set_down netif_set_down_STUBBED
+#define netif_set_up netif_set_up_STUBBED
+#define pbuf_alloc pbuf_alloc_STUBBED
+#define pbuf_free pbuf_free_STUBBED
+#define pbuf_ref pbuf_ref_STUBBED
+#define sys_check_timeouts sys_check_timeouts_STUBBED
+#define sys_timeout sys_timeout_STUBBED
+#define sys_untimeout sys_untimeout_STUBBED
+
+#endif // LWIP_STUBBED_SYMBOLS_H
+
